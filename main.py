6b80efd6974b31a8cd60ceea73a7aa8822fd33e8
pythonpaul <paulsun096@gmail.com>
2024-12-20 16:26:56 +0000

    added drop down for client selection

--- main.py
+++ main.py
@@ -6,12 +6,24 @@ from js import console
 
 title = "Pandas (and basic DOM manipulation)"
 page_message = "This example loads a remote CSV file into a Pandas dataframe, and displays it."
-url = "https://raw.githubusercontent.com/datasets/airport-codes/master/data/airport-codes.csv"
+client_urls = {
+    "Flounders": "https://raw.githubusercontent.com/datasets/airport-codes/master/data/airport-codes.csv",
+    "Biocell": "https://people.sc.fsu.edu/~jburkardt/data/csv/hw_200.csv"
+}
 
 pydom["title#header-title"].html = title
 pydom["a#page-title"].html = title
 pydom["div#page-message"].html = page_message
-pydom["input#txt-url"][0].value = url
+
+# Add dropdown widget dynamically
+dropdown_html = "<select id='client-dropdown'>" + "".join(
+    f"<option value='{url}'>{client}</option>" for client, url in client_urls.items()
+) + "</select>"
+pydom["div#dropdown-container"].html = dropdown_html
+
+# Set initial URL to the first option
+initial_url = list(client_urls.values())[0]
+pydom["input#txt-url"].value = initial_url
 
 def log(message):
     # log to pandas dev console
@@ -21,7 +33,7 @@ def log(message):
 
 def loadFromURL(event):
     pydom["div#pandas-output-inner"].html = ""
-    url = pydom["input#txt-url"][0].value
+    url = pydom["#client-dropdown"].value
 
     log(f"Trying to fetch CSV from {url}")
     df = pd.read_csv(open_url(url))
@@ -30,3 +42,6 @@ def loadFromURL(event):
     pydom["div#pandas-dev-console"].style["display"] = "block"
 
     display(df, target="pandas-output-inner", append="False")
+
+# Attach event listener to dropdown
+pydom["#client-dropdown"].on("change", loadFromURL)
